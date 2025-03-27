23e710484438274a84757e86340c24a9804e34f7
wadkisson <adkisson@wustl.edu>
2025-03-27 14:34:20 -0500

    fixed type

--- fineweb_loader.py
+++ fineweb_loader.py
@@ -10,7 +10,7 @@ tokenizer = tiktoken.get_encoding("gpt2")
 end_of_text_token = tokenizer._special_tokens['<|endoftext|>']
 
 local_directory = "edu_fineweb10B"
-remote_name = "sampe-10BT"
+remote_name = "sample-10BT"
 shard_size = int(1e8)
 DATA_DIR = os.path.join(os.path.dirname(__file__), local_directory)
 fw = load_dataset("HuggingFaceFW/fineweb-edu",name = remote_name,split = "train")
